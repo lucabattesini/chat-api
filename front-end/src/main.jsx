0b03eb1f78164195baaded48eb9488015a55ecfb
lucabattesini <lucabattesini1@gmail.com>
2026-03-11 16:07:18 -0300

    Setting up chacka on the main page

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import ReactDOM from "react-dom/client"
+import App from "./App"
+import { ChakraProvider } from "@chakra-ui/react"
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <ChakraProvider>
     <App />
-  </StrictMode>,
+  </ChakraProvider>
 )
