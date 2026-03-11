fc4cb0c1be5ae5fa875ba19ec52607730032fb79
lucabattesini <lucabattesini1@gmail.com>
2026-03-11 15:57:47 -0300

    Organizing front and back end

---
+++ front-end/src/components/chat-card.jsx
@@ -0,0 +1,5 @@
+export default function ChatCard (content) {
+    return (
+        <h1>Content</h1>
+    );
+}
---
+++ front-end/src/pages/chat.jsx
@@ -0,0 +1,8 @@
+export default function Chat() {
+    return (
+        <div>
+            <h1>User Name</h1>
+            <input type="text"></input>
+        </div>
+    );
+}
